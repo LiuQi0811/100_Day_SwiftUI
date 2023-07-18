5618993d790b04cfaeef04ca625a1c028adb5406
LIUQI <1139642444@qq.com>
2023-07-18 13:24:04 +0800

    项目初始化

---
+++ WeSplitApp/WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by 刘奇 on 2023/7/18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
