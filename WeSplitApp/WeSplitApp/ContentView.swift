a8c60129e28b06ce8df9a9e6fa6861383197e820
LIUQI <1139642444@qq.com>
2023-07-18 13:26:00 +0800

    新增 按钮  @State 修改程序状态 导航栏等

--- WeSplitApp/WeSplitApp/ContentView.swift
+++ WeSplitApp/WeSplitApp/ContentView.swift
@@ -2,20 +2,57 @@
 //  ContentView.swift
 //  WeSplitApp
 //
-//  Created by 刘奇 on 2023/7/18.
+//  Created by 刘奇 on 2023/7/17.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    // 计数值
+    // @State允许我们绕过结构的限制 修改程序状态
+    @State private var count = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // 创建导航栏视图
+        NavigationView{
+            // 创建Form 表单
+             Form{
+                 // 创建表单拆分为离散的视觉组
+                 Section{
+                     // 创建文本视图
+                     Text("大杯")
+                         // 边距填充
+                         .padding()
+                 }
+                 
+                 Section{
+                     // 创建文本视图
+                     Text("中杯")
+                         // 边距填充
+                         .padding()
+                 }
+                 Section{
+                     // 创建文本视图
+                     Text("小杯")
+                         // 边距填充
+                         .padding()
+                 }
+                 Section{
+                     // 创建计数按钮
+                     Button("点击次数 \(count)"){
+                         self.count += 1
+                     }
+                     // 创建重置按钮
+                     Button("重置次数"){
+                         self.count  = 0
+                     }
+                 }
+                 
+             }
+            // 导航栏标题
+             .navigationTitle("规格")
+            // 导航栏标题样式 小 居中
+             .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
