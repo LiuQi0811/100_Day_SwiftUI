cb5226fee4daea38e5dea028f5965474538a10c6
LIUQI <1139642444@qq.com>
2023-07-20 13:15:28 +0800

    猜国旗项目初始化

---
+++ GuessingNationalFlag/GuessingNationalFlag/GuessingNationalFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingNationalFlagApp.swift
+//  GuessingNationalFlag
+//
+//  Created by 刘奇 on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessingNationalFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
