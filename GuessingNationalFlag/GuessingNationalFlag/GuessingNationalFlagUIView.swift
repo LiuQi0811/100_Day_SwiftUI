c2cf31cb907473cd0d0852a4db4ace72f264aa29
LIUQI <1139642444@qq.com>
2023-08-12 14:14:19 +0800

    新增重置/重新洗牌方法函数 回答答案验证方法函数 相关

--- GuessingNationalFlag/GuessingNationalFlag/GuessingNationalFlagUIView.swift
+++ GuessingNationalFlag/GuessingNationalFlag/GuessingNationalFlagUIView.swift
@@ -8,29 +8,76 @@
 import SwiftUI
 
 struct GuessingNationalFlagUIView: View {
-    // 国家数组
-    var countries =  ["爱沙尼亚","法国","德国","爱尔兰","意大利","尼日利亚","波兰","俄罗斯","西班牙","英国","美国"]
+    // 国家数组  shuffled()方法会自动为我们随机化数组顺序
+    @State var countries =  ["爱沙尼亚","法国","德国","爱尔兰","意大利","尼日利亚","波兰","俄罗斯","西班牙","英国","美国"].shuffled()
     // 随机取值
-    var correctAnswer = Int.random(in: 0...2)
+    @State var correctAnswer = Int.random(in: 0...2)
+    // 展示得分成绩标识
+    @State private var showingScore = false
+    // 得分成绩标题
+    @State private var scoreTitle = ""
     var body: some View {
-        // 创建垂直堆栈视图
-        VStack(spacing: 30){
-            VStack{
-                Text(countries[correctAnswer])
-            }
+        // 创建水平堆栈视图
+        ZStack{
             
-            ForEach(0..<3){item in
-                Button{
-                    
-                }label: {
-                    Image(usTranslateEnByImage(countries:countries)[item])
-                        // 渲染原始图像像素
-                        .renderingMode(.original)
+            // 创建背景颜色
+            Color.blue
+                // 创建安全域
+                .ignoresSafeArea()
+            
+            // 创建垂直堆栈视图
+            VStack(spacing: 30){
+                VStack{
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                   
+                }
+                // 循环遍历旗帜
+                ForEach(0..<3){item in
+                    // 创建旗帜按钮
+                    Button{
+                        self.answerCheck(item)
+                    }label: {
+                        Image(usTranslateEnByImage(countries:countries)[item])
+                            // 渲染原始图像像素
+                            .renderingMode(.original)
+                    }
                 }
             }
         }
+        // 创建提示弹框
+        .alert(self.scoreTitle,isPresented: self.$showingScore){
+            
+        }message: {
+            Text("你的成绩是：")
+        }
+       
+    }
+    
+    /**
+     重置/重新洗牌
+     */
+    func reshuffle(){
+        // shuffled()方法会自动为我们随机化数组顺序
+        self.countries.shuffle()
+        self.correctAnswer = Int.random(in: 0...2)
+    }
+    
+    /**
+     回答答案验证
+     */
+    func answerCheck(_ number: Int){
+        // 答案对比
+        if(number == self.correctAnswer){
+            self.scoreTitle = "回答正确"
+        }else{
+            self.scoreTitle = "回答错误"
+        }
+        // 得分成绩展示
+        self.showingScore = true
     }
 }
+
 
 /**
   中英文翻译 用于国旗展示
